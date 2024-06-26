90e67f7a3c9cf8578759e25fa44739d15747c0eb
Macenzi <nomile>
2024-06-26 22:42:32 +0500

    Initial

---
+++ ezFERM - создатель ферм/Шаблон/Проект VS/Actions/Задача_1.cs
@@ -0,0 +1,16 @@
+﻿using ezFERM.Interfaces;
+using static ezFERM.Settings.PMSettings;
+using static ezFERM.API.API;
+
+namespace ZennoPosterProject1.Actions
+{
+    public class Задача_1 : IAction
+    {
+        // Имя задачи = имя класса.
+        // Строки кода вашей задачи внутри тела метода RunAction().
+        public WorkResult RunAction()
+        {
+            return WorkResult.Удачно;
+        }
+    }
+}
---
+++ ezFERM - создатель ферм/Шаблон/Проект VS/Actions/Задача_2.cs
@@ -0,0 +1,14 @@
+﻿using ezFERM.Interfaces;
+using static ezFERM.Settings.PMSettings;
+using static ezFERM.API.API;
+
+namespace ZennoPosterProject1.Actions
+{
+    public class Задача_2 : IAction
+    {
+        public WorkResult RunAction()
+        {
+            return WorkResult.Удачно;
+        }
+    }
+}
